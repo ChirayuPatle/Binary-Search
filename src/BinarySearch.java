877f0a7a289c63ac7e4c77d1ba0981c7634e2291
Chirayupatle <chirayu.patle@vectorlab.in>
2026-03-02 11:19:29 +0530

    feat:binary_search_code

---
+++ src/BinarySearch.java
@@ -0,0 +1,36 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+    int[] arr = {1,8,12,15,20,24,36,42,58};
+    int target = 20;
+    int ans = binarysearch(arr, target);
+    System.out.println(ans);
+    }
+
+    static  int binarysearch(int[] arr, int target){
+        // step 1 - set start, end and middle element
+        int start = 0;
+        int end = arr.length -1;
+
+        while (start <= end) {
+//            int mid = (start + end) /2 ; // this will cause error when large input is taken
+            int mid = start + (end - start) / 2;
+
+            // check for target and adjust start, mid, end element
+            if(target < arr[mid]){ // left side search
+                end = mid -1;
+            } else if (target > arr[mid]) { // right side search
+                start = mid +1;
+            }
+            else{ // target == mid --> result
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+/*
+ Steps:
+ 1. set start, end and middle element.
+ 2. adjust start, mid, end element
+ */
